b3479adc3d53d644fdb8f18f3825f28362df9144
Tej MH <tejhagargigit@gmail.com>
2024-07-05 23:23:51 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,34 @@
-const express = require('express');
-const http = require('http');
-const socketio = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
-    cors: {
-        origin: "*"
-    }
+  cors: {
+    origin: "*",
+  },
 });
 
 let activeUsers = {};
 
 io.on("connection", (socket) => {
-    console.log('Connected');
+  console.log("Connected");
 
-    socket.on("join", (userName) => {
-        activeUsers[socket.id] = userName;
-        io.emit("activeUsers", Object.values(activeUsers));
-    }); 
+  socket.on("join", (userName) => {
+    activeUsers[socket.id] = userName;
+    io.emit("activeUsers", Object.values(activeUsers));
+  });
 
-    socket.on("chat", (payload) => {
-        socket.broadcast.emit("chat", payload); 
-    
-    socket.on('disconnect', () => {
-        console.log('Disconnected');
-        delete activeUsers[socket.id];
-        io.emit("activeUsers", Object.values(activeUsers));
+  socket.on("chat", (payload) => {
+    socket.broadcast.emit("chat", payload);
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected");
+      delete activeUsers[socket.id];
+      io.emit("activeUsers", Object.values(activeUsers));
     });
+  });
 });
 
-server.listen(3000, () => console.log('Server is listening at port 3000'));
+server.listen(3000, () => console.log("Server is listening at port 3000"));
