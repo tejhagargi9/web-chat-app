0a89f9cdc5c35ca7b22f00411929973b2a8a70ff
Tej MH <tejhagargigit@gmail.com>
2024-07-06 16:52:46 +0530

    cors implemented

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: "*",
+    origin: "*", // Allow requests from all origins
+    methods: ["GET", "POST"], // Allow specific HTTP methods
+    allowedHeaders: ["Content-Type"], // Allow specific headers
+    credentials: true, // Allow cookies to be sent with requests
   },
 });
 
+// Use the cors middleware
+app.use(cors());
+
 let activeUsers = {};
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 io.on("connection", (socket) => {
   console.log("Connected");
@@ -22,12 +33,12 @@ io.on("connection", (socket) => {
 
   socket.on("chat", (payload) => {
     socket.broadcast.emit("chat", payload);
+  });
 
-    socket.on("disconnect", () => {
-      console.log("Disconnected");
-      delete activeUsers[socket.id];
-      io.emit("activeUsers", Object.values(activeUsers));
-    });
+  socket.on("disconnect", () => {
+    console.log("Disconnected");
+    delete activeUsers[socket.id];
+    io.emit("activeUsers", Object.values(activeUsers));
   });
 });
 
