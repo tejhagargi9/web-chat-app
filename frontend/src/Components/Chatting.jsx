555a4e0780dd5040cf8b2682036aee78721fa521
Tej MH <tejhagargigit@gmail.com>
2024-07-06 18:12:55 +0530

    all settttt

--- frontend/src/Components/Chatting.jsx
+++ frontend/src/Components/Chatting.jsx
@@ -14,7 +14,7 @@ const Chatting = () => {
   const socket = useRef();
 
   useEffect(() => {
-    socket.current = io("http://localhost:3000", {
+    socket.current = io("https://web-chat-app-gold.vercel.app", {
       transports: ["websocket", "polling"],
       withCredentials: true,
     }); // Ensure this is the correct URL without trailing slash
