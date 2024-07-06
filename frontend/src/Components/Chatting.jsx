3f6adff6d8cc1dcbc6c9dab0364c78b7a21769d2
Tej MH <tejhagargigit@gmail.com>
2024-07-06 17:33:56 +0530

    all sett

--- frontend/src/Components/Chatting.jsx
+++ frontend/src/Components/Chatting.jsx
@@ -14,7 +14,7 @@ const Chatting = () => {
   const socket = useRef();
 
   useEffect(() => {
-    socket.current = io("http://localhost:3000/");
+    socket.current = io("https://web-chat-app-gold.vercel.app/");
 
     socket.current.on("chat", (msg) => {
       setMessages((prevMessages) => [...prevMessages, msg]);
